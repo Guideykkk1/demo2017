6f78d387b22a2ed2bd329c93e8a517066c993491
pawat sawangsuk <pawat.sawa@gmail.com>
2017-01-28 16:56:57 +0700

    demo2017

---
+++ Demo2017/Assets/Script/sceneball.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class sceneball : MonoBehaviour {
+	public Text txtScore;
+	public Text txtattack;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	int Score = 0;
+	int attack = 0;
+	public void DoPushButton(){
+		Score++;
+		attack++;
+		txtattack.text = "Score : " + attack.ToString ();
+		txtScore.text = "Score : " + Score.ToString ();
+	}
+	public void DoChangScene(){
+		SceneManager.LoadScene (1);
+	}
+}
